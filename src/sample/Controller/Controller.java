cbde812cf68280b043e61306be74465e087a038b
christianekenstedt <christian>
2016-10-11 14:21:19 +0200

    Initial commit

---
+++ src/sample/Controller/Controller.java
@@ -0,0 +1,32 @@
+package sample.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private Button declineBtn;
+
+    @FXML
+    private Button callBtn;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    void callBtnPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void declineBtnPressed(ActionEvent event) {
+
+    }
+
+}
