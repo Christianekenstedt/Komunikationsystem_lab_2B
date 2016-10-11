343553da19fc58fa04a2f6bd910c12b9a5438f6a
christianekenstedt <christian>
2016-10-11 16:55:02 +0200

    structure and rename of view. prepared to give controller reference.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,32 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import sample.Controller.Controller;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/client.fxml"));
+        Parent root = loader.load();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        Controller controller = loader.getController();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                // Do close stuff.
+            }
+        });
+
         primaryStage.show();
     }
 
